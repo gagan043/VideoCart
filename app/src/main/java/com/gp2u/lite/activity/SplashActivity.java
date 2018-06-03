c597d8cbc56f34b49ae51dfde8c3a16ce005c4b6
James Freeman <airmedical@gmail.com>
2018-06-03 20:14:48 +1000

    Added new splash and animation

--- app/src/main/java/com/gp2u/lite/activity/SplashActivity.java
+++ app/src/main/java/com/gp2u/lite/activity/SplashActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.gp2u.lite.R;
 import com.gp2u.lite.control.AudioRouter;
@@ -20,6 +23,10 @@ public class SplashActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
         AudioRouter.startAudioRouting(this);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Animation hyperspaceJump = AnimationUtils.loadAnimation(this, R.anim.hyperspace_jump);
+        imageView.startAnimation(hyperspaceJump);
 
         new Handler().postDelayed(new Runnable(){
             @Override

2f41c7c4b8ae471eb0567c32c11baf35fcb7789b
worksdelight <poras.dhiman@worksdelight.com>
2018-07-03 10:24:57 +0530

    start work on seller side

---
+++ app/src/main/java/com/admin/videocart/activity/SellerSettingActivity.java
@@ -0,0 +1,15 @@
+package com.admin.videocart.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.admin.videocart.R;
+
+public class SellerSettingActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting_layout);
+    }
+}
